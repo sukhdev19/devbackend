aa200de1fb1a922bcb0f3c4d0db9cd6345334f97
sukhdev Soniyata <soniyatadev@gmail.com>
2024-08-30 00:31:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/twitter',(req,res)=>{
+    res.send('tarde trends')
+})
+app.get('/login',(req,res)=>{
+    res.send('<h1> please login at chai and code</h1>')
+})
+app.get('/youtube',(req,res)=>{
+    res.send('<h2> chai or code</h2>')
+})
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
